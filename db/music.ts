7049be061b6e08151fd24e58d87378f9c4931a5b
Craig D'Silva <craig.dsilva10@gmail.com>
2026-01-05 19:31:28 +0000

    Fetch single music from DB

--- db/music.ts
+++ db/music.ts
@@ -1,7 +1,15 @@
+import { ObjectId } from "mongodb";
+
 import db from "./db";
 
 export const getAllMusic = async () => {
   const collection = await db.collection("music");
   const results = await collection.find({}).toArray();
+  return results;
+};
+
+export const getMusic = async (id: string) => {
+  const collection = await db.collection("music");
+  const results = await collection.findOne({ _id: new ObjectId(id) });
   return results;
 };
