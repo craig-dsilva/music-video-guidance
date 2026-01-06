3be15cbf1eb1bbe9b05bcc112ba20763d24bae6e
Craig D'Silva <craig.dsilva10@gmail.com>
2026-01-06 21:22:12 +0000

    Update metadata with browser icon and page description

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Music Video Guidance",
-  description: "",
+  description: "This website is a parental guidance for music videos.",
+  icons: { icon: "/music.png" },
 };
 
 const RootLayout = ({
