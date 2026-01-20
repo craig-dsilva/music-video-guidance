9a9c0ee1f945d7ae7fe2f0e3b1c66bf6bcef3ea3
Craig D'Silva <craig.dsilva10@gmail.com>
2026-01-20 15:53:01 +0000

    Create GET APIs

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,13 +1,13 @@
 import dbConnect from "@/lib/mongodb";
 import Music from "@/models/music";
 
-export const GET = async (req, res) => {
+export const GET = async () => {
   await dbConnect();
 
   try {
     const musics = await Music.find({});
-    return Response.json({ success: true, data: musics });
+    return Response.json({ success: true, data: musics, count: musics.length });
   } catch (error) {
-    return Response.json({ success: false });
+    return Response.json({ success: false, error });
   }
 };
