143837734a636e0426fbe71337845a15b358a086
Craig D'Silva <craig.dsilva10@gmail.com>
2026-01-21 14:49:35 +0000

    Add POST to api/music

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,12 +1,22 @@
 import dbConnect from "@/lib/mongodb";
 import Music from "@/models/music";
 
-export const GET = async () => {
-  await dbConnect();
+await dbConnect();
 
+export const GET = async () => {
   try {
     const musics = await Music.find({});
     return Response.json({ success: true, data: musics, count: musics.length });
+  } catch (error) {
+    return Response.json({ success: false, error });
+  }
+};
+
+export const POST = async (req: Request) => {
+  const res = await req.json();
+  try {
+    await Music.insertOne(res);
+    return Response.json({ success: true, data: res });
   } catch (error) {
     return Response.json({ success: false, error });
   }
