021f224b6a7041aaf939934bdd077cab94d5e521
Craig D'Silva <craigdsilva@proton.me>
2026-07-22 10:48:20 +0100

    Create client

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="py-5 text-center">
-      <p>&copy; {new Date().getFullYear()} Music Video Guidance</p>
-    </footer>
-  );
-};
-
-export default Footer;
